b497f6a4df04bd2c5ba188080a3469b1bf02f3d0
Илья <camypai.13@list.ru>
2019-02-04 01:01:11 +0300

    Use ICollision interface and GameObjectException

--- CompGame/Models/BaseObject.cs
+++ CompGame/Models/BaseObject.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Drawing;
+using CompGame.Exceptions;
+using CompGame.Interfaces;
 
 namespace CompGame.Models
 {
-    public abstract class BaseObject
+    public abstract class BaseObject : ICollision
     {
         protected Point Pos;
         protected Point Dir;
         protected Size Size;
+
+        public Rectangle Rectangle => new Rectangle(Pos, Size);
 
         /// <summary>
         /// Инициализация объекта
@@ -17,6 +21,21 @@ namespace CompGame.Models
         /// <param name="size">Размер</param>
         protected BaseObject(Point pos, Point dir, Size size)
         {
+            if (pos.X < 0 || pos.Y < 0 || pos.X > Scene.Width || pos.Y > Scene.Height)
+            {
+                throw new GameObjectException("Не верно задана позиция");
+            }
+
+            if (size.Width < 0 || size.Height < 0)
+            {
+                throw new GameObjectException("Не верно задан размер");
+            }
+
+            if (dir.X > 80 || dir.Y > 80 || dir.X < -80 || dir.Y < -80)
+            {
+                throw new GameObjectException("Не верно задано смещение");
+            }
+
             Pos = pos;
             Dir = dir;
             Size = size;
@@ -31,5 +50,12 @@ namespace CompGame.Models
         /// Обновление положения объекта
         /// </summary>
         public abstract void Update();
+
+        public bool Collision(ICollision collision)
+        {
+            return collision.Rectangle.IntersectsWith(Rectangle);
+        }
+
+        public abstract void Reload();
     }
 }
