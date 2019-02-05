5a68600ddc1015e4759120d9482ac83c76d480d5
Илья <camypai.13@list.ru>
2019-02-05 22:03:05 +0300

    Add documentation

--- CompGame/Models/Asteroid.cs
+++ CompGame/Models/Asteroid.cs
@@ -5,7 +5,14 @@ namespace CompGame.Models
 {
     public class Asteroid : BaseObject, ICloneable, IComparable<Asteroid>
     {
+        /// <summary>
+        /// Изображение
+        /// </summary>
         private readonly Image _asteroid;
+        
+        /// <summary>
+        /// Сила
+        /// </summary>
         public int Power { get; set; } = 3;
 
         public Asteroid(Point pos, Point dir, Size size) : base(pos, dir, size)
@@ -16,22 +23,35 @@ namespace CompGame.Models
             Power = 1;
         }
 
+        /// <summary>
+        /// Отрисовка экземпляра
+        /// </summary>
         public override void Draw()
         {
             Scene.Buffer.Graphics.DrawImage(_asteroid, Pos);
         }
 
+        /// <summary>
+        /// Обновление положения экземпляра
+        /// </summary>
         public override void Update()
         {
             Pos.X = Pos.X + Dir.X;
             if (Pos.X < 0) Reload();
         }
 
+        /// <summary>
+        /// Перезагрузка положения экземпляра
+        /// </summary>
         public override void Reload()
         {
             Pos = new Point(Scene.Width, new Random().Next(Scene.Height));
         }
 
+        /// <summary>
+        /// Клонирование экземпляра
+        /// </summary>
+        /// <returns>Новый экземпляр</returns>
         public object Clone()
         {
             var asteroid = new Asteroid(new Point(Pos.X, Pos.Y), new Point(Dir.X, Dir.Y),
@@ -42,6 +62,11 @@ namespace CompGame.Models
             return asteroid;
         }
 
+        /// <summary>
+        /// Сравнение экземпляра
+        /// </summary>
+        /// <param name="asteroid">Объект сравнения</param>
+        /// <returns></returns>
         public int CompareTo(Asteroid asteroid)
         {
             if (Power > asteroid.Power)
