4e4955d25308ad32e5938e160390cc55b15bd92d
Илья <camypai.13@list.ru>
2019-02-07 15:11:04 +0300

    Add documentation and add logging

--- CompGame/Models/Bullet.cs
+++ CompGame/Models/Bullet.cs
@@ -1,27 +1,48 @@
+using System;
 using System.Drawing;
 
 namespace CompGame.Models
 {
     public class Bullet : BaseObject
     {
-        public Bullet(Point pos, Point dir, Size size) : base(pos, dir, size)
+        /// <summary>
+        /// Инициализация пули
+        /// </summary>
+        /// <param name="pos">Позиция</param>
+        /// <param name="dir">Смещение</param>
+        /// <param name="size">Размер</param>
+        /// <param name="log">Метод логгирования</param>
+        public Bullet(Point pos, Point dir, Size size, EventHandler<string> log) : base(pos, dir, size, log)
         {
+            Logging(this, "Создан");
         }
 
+        /// <summary>
+        /// Отрисовка пули
+        /// </summary>
         public override void Draw()
         {
             Scene.Buffer.Graphics.DrawRectangle(Pens.OrangeRed, Pos.X, Pos.Y, Size.Width, Size.Height);
+            Logging(this, "Отрисован");
         }
 
+        /// <summary>
+        /// Изменение положения полу
+        /// </summary>
         public override void Update()
         {
             Pos.X = Pos.X + 3;
             if(Pos.X > Scene.Width) Reload();
+            Logging(this, "изменилось положение");
         }
 
+        /// <summary>
+        /// Перезагрузка объекта пули
+        /// </summary>
         public override void Reload()
         {
             Pos.X = 0;
+            Logging(this, "Перезагрузка");
         }
     }
 }
--- CompGame/Models/Star.cs
+++ CompGame/Models/Star.cs
@@ -5,26 +5,46 @@ namespace CompGame.Models
 {
     public class Star : BaseObject
     {
-        public Star(Point pos, Point dir, Size size) : base(pos, dir, size)
+        /// <summary>
+        /// Инициализация звезды
+        /// </summary>
+        /// <param name="pos">Позиция</param>
+        /// <param name="dir">Смещение</param>
+        /// <param name="size">Размер</param>
+        /// <param name="log">Метод логгирования</param>
+        public Star(Point pos, Point dir, Size size, EventHandler<string> log) : base(pos, dir, size, log)
         {
+            Logging(this, "Создан");
         }
 
+        /// <summary>
+        /// Отрисовка звезды
+        /// </summary>
         public override void Draw()
         {
             Scene.Buffer.Graphics.DrawLine(Pens.White, Pos.X, Pos.Y, Pos.X + Size.Width, Pos.Y + Size.Height);
             Scene.Buffer.Graphics.DrawLine(Pens.White, Pos.X + Size.Width, Pos.Y, Pos.X, Pos.Y + Size.Height);
+            Logging(this, "Отрисован");
         }
 
+        /// <summary>
+        /// Изменение положения звезды
+        /// </summary>
         public override void Update()
         {
             // Заменил минус на плюс, потому что двигались они в другую сторону
             Pos.X = Pos.X + Dir.X;
             if (Pos.X < 0) Reload();
+            Logging(this, "Изменилось положение");
         }
 
+        /// <summary>
+        /// Перезагрузка объекта звезды
+        /// </summary>
         public override void Reload()
         {
             Pos = new Point(Scene.Width, new Random().Next(Scene.Height));
+            Logging(this, "Перезагрузка");
         }
     }
 }
